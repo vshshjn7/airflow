dd9be4b10641b5d493d672982c66dec30d4783b4
vshshjn7 <vshshjn7@gmail.com>
2020-07-27 18:28:51 +0530

    [CP][EWT-418][AIRFLOW-6840] Bump up version of future (#7471) (#55)
    * [CP][AIRFLOW-6840] Bump up version of future (#7471)
    
    * version bump
    
    Co-authored-by: yuqian90 <yuqian1990@gmail.com>
    Co-authored-by: Vishesh Jain <visheshj@twitter.com>

--- setup.py
+++ setup.py
@@ -340,7 +340,7 @@ def do_setup():
             'flask-swagger==0.2.13',
             'flask-wtf>=0.14.2, <0.15',
             'funcsigs==1.0.0, <2.0.0',
-            'future>=0.16.0, <0.17',
+            'future>=0.16.0, <0.19',
             'gunicorn>=19.5.0, <20.0',
             'iso8601>=0.1.12',
             'json-merge-patch==0.2',
