c7da21a593c822feaad02f7317fa8320c0a2b9cd
Vishesh Jain <visheshj@twitter.com>
2020-09-01 19:15:53 +0530

    [EWT-450] fixing sla miss triggering duplicate alerts every minute

--- airflow/migrations/versions/a982338e6d7f_sla_miss_execution_time_on_update_.py
+++ airflow/migrations/versions/a982338e6d7f_sla_miss_execution_time_on_update_.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -16,7 +15,33 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
 
-version = '1.10.4+twtr16'
+"""sla miss execution_time on update current time fix
 
+Revision ID: a982338e6d7f
+Revises: 0e2a74e0fc9f
+Create Date: 2020-09-01 13:48:03.093594
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'a982338e6d7f'
+down_revision = '0e2a74e0fc9f'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+from sqlalchemy.dialects import mysql
+from sqlalchemy import text
+
+
+def upgrade():
+    conn = op.get_bind()
+    if conn.dialect.name == "mysql":
+        op.alter_column(table_name='sla_miss', column_name='execution_date',
+            type_=mysql.TIMESTAMP(fsp=6), nullable=False,
+            server_default=text('CURRENT_TIMESTAMP(6)'))
+
+
+def downgrade():
+    pass
