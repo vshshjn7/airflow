ed3a9d2fedd82d6be08dcc3dd7c204558afa872f
Vishesh Jain <visheshj@twitter.com>
2020-09-02 15:51:09 +0530

    issue description

--- airflow/migrations/versions/a982338e6d7f_sla_miss_execution_time_on_update_.py
+++ airflow/migrations/versions/a982338e6d7f_sla_miss_execution_time_on_update_.py
@@ -38,6 +38,36 @@ from sqlalchemy import text
 def upgrade():
     conn = op.get_bind()
     if conn.dialect.name == "mysql":
+        # NOTE:
+        #
+        # This patch is internal to Twitter, Here we explicitly set a default to the string literal
+        # `CURRENT_TIMESTAMP(6)` to avoid the
+        # default MySQL behavior for TIMESTAMP without `explicit_defaults_for_timestamp` turned
+        # on as stated here:
+        #
+        #  "The first TIMESTAMP column in a table, if not explicitly declared with the NULL
+        #  attribute or an explicit
+        #   DEFAULT or ON UPDATE attribute, is automatically declared with the DEFAULT
+        #   CURRENT_TIMESTAMP and
+        #   ON UPDATE CURRENT_TIMESTAMP attributes." [0]
+        #
+        # Because of the "ON UPDATE CURRENT_TIMESTAMP" default, anytime the `sla_miss` table
+        # is UPDATE'd without
+        # explicitly re-passing the current value for the `execution_date` column, it will end up
+        # getting clobbered with
+        # the current timestamp value which breaks sla functionality and
+        # causes duplicate alerts every minute.
+        #
+        # We unfortunately cannot turn `explicit_defaults_for_timestamp` on globally ourselves as
+        # is now technically
+        # required by Airflow [1], because this has to be set in the my.cnf and we don't control
+        # that in managed MySQL.
+        # A request to enable this fleet-wide has been made in MVP-18609.
+        #
+        # [0]: https://dev.mysql.com/doc/refman/5.6/en/server-system-variables.html
+        # #sysvar_explicit_defaults_for_timestamp
+        # [1]: https://github.com/apache/incubator-airflow/blob/master/UPDATING.md#mysql-setting
+        # -required
         op.alter_column(table_name='sla_miss', column_name='execution_date',
             type_=mysql.TIMESTAMP(fsp=6), nullable=False,
             server_default=text('CURRENT_TIMESTAMP(6)'))
